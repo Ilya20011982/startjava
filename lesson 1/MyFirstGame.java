27d587403843731aa90be9dd7c507cd4172e7fac
Ilya20011982 <ilya_fr@mail.ru>
2021-05-26 23:23:08 +0500

    first

--- lesson 1/MyFirstGame.java
+++ lesson 1/MyFirstGame.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 public class MyFirstGame {
     public static void main(String[] args) {
         Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        System.out.println(randomNumber);
+        int randomNumber = random.nextInt(101);
         Scanner in = new Scanner(System.in);
         System.out.print("Введите целое число от 0 до 100 которое загадал компьютер - ");
-        int number = in.nextInt();
-        while (number != randomNumber) {
-            if (number > randomNumber) {
+        int  playerNumber = in.nextInt();
+        while (playerNumber != randomNumber) {
+            if (playerNumber > randomNumber) {
                 System.out.print("Ваше число БОЛЬШЕ, введите число - ");
             } else {
                 System.out.print("Ваше число МЕНЬШЕ, введите число - ");
             }
-            number = in.nextInt();
+            playerNumber = in.nextInt();
         }
-        System.out.print("Вы угадали число - " + number);
+        System.out.print("Вы угадали число - " + playerNumber);
     }
 }
