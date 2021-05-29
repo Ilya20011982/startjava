b6bcf395b164edfdf68bc642fd5ee4b7390010b7
Ilya20011982 <ilya_fr@mail.ru>
2021-05-29 23:55:22 +0500

    add

---
+++ lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String gender = "male";
+    String name = "Sam";
+    double height = 1.85;
+    float weight = 80f;
+    int age = 30;
+
+    void move() {
+        System.out.println("moving");
+    }
+
+    String sitDown() {
+        return "sitting";
+    }
+
+    void run() {
+        System.out.println("running");
+    }
+
+    String speak() {
+        return "speaking";
+    }
+
+    void learn() {
+        System.out.println("lerning Java")
+    }
+}
