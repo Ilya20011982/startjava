2547e094bf583f9388649af3b6b50554b1039edd
Ilya20011982 <ilya_fr@mail.ru>
2021-06-02 19:46:08 +0500

    add

---
+++ lesson 2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+
+    String nickname;
+    String gender;
+    String color;
+    double weight;
+    int age;
+
+    void walk() {
+        System.out.println("walking");
+    }
+
+    String sitDown() {
+        return "sitting";
+    }
+
+    void howl() {
+        System.out.println("howling");
+    }
+
+    String run() {
+        return "running";
+    }
+
+    void hunt() {
+        System.out.println("hunting");
+    }
+}
