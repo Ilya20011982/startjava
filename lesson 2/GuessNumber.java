ac27cddac208b7c841611f347cc78434b6923ed0
Ilya20011982 <ilya_fr@mail.ru>
2021-06-10 23:32:22 +0500

    add

--- lesson 2/GuessNumber.java
+++ lesson 2/GuessNumber.java
@@ -1,32 +1,50 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessNumber {
     private final int randomNumber;
-    private String nameWinner;
+    private Player firstPlayer;
+    private Player secondPlayer;
+    private String winner;
 
-    public GuessNumber() {
+    public GuessNumber(Player firstPlayer, Player secondPlayer) {
+        this.firstPlayer = firstPlayer;
+        this.secondPlayer= secondPlayer;
+        System.out.println("Компьютер загадал число от 0 до 100");
         Random random = new Random();
         randomNumber = random.nextInt(101);
     }
 
-    public void setNameWinner(String nameWinner) {
-        this.nameWinner = nameWinner;
+    public String getWinner() {
+        return winner;
     }
 
-    public String getNameWinner() {
-        return nameWinner;
+    public void setWinner(String winner) {
+        this.winner = winner;
     }
 
     public boolean isNumber(int number) {
         if (randomNumber < number) {
-            System.out.println("Ваше число БОЛЬШЕ, введите число - ");
+            System.out.print("Ваше число БОЛЬШЕ, введите число - ");
             return false;
         }
         if (randomNumber > number) {
-            System.out.println("Ваше число МЕНЬШЕ, введите число - ");
+            System.out.print("Ваше число МЕНЬШЕ, введите число - ");
             return false;
         }
         System.out.println("Вы угадали число - " + number);
             return true;
+    }
+
+    public void insertNumber1() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Игрок " + firstPlayer.getName() + " введите число - ");
+        firstPlayer.setNumber(in.nextInt());
+    }
+
+    public void insertNumber2() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Игрок " + secondPlayer.getName() + " введите число - ");
+        secondPlayer.setNumber(in.nextInt());
     }
 }
--- lesson 2/GuessNumberTest.java
+++ lesson 2/GuessNumberTest.java
@@ -5,39 +5,35 @@ public class GuessNumberTest {
         Scanner in = new Scanner(System.in);
         System.out.println(("Добро пожаловать в игру 'Угадай число'!"));
 
-        System.out.println("Введите имя первого игрока - ");
+        System.out.print("Введите имя первого игрока - ");
         Player firstPlayer = new Player(in.next());
 
-        System.out.println("Введите имя второго игрока - ");
+        System.out.print("Введите имя второго игрока - ");
         Player secondPlayer = new Player(in.next());
 
         String playerAnswer = "yes";
 
         while ("yes".equals(playerAnswer)) {
-            System.out.println("Компьютер загадал число от 0 до 100");
-            GuessNumber game = new GuessNumber();
+            GuessNumber game = new GuessNumber(firstPlayer, secondPlayer);
 
             while (true) {
-                System.out.println("Игрок " + firstPlayer.getName() + " введите число - ");
-                firstPlayer.setNumber(in.nextInt());
+                game.insertNumber1();
                 if (game.isNumber(firstPlayer.getNumber())) {
-                    game.setNameWinner(firstPlayer.getName());
+                    game.setWinner(firstPlayer.getName());
                     break;
                 }
-                System.out.println("Игрок " + secondPlayer.getName() + " введите число - ");
-                secondPlayer.setNumber(in.nextInt());
+                game.insertNumber2();
                 if (game.isNumber(secondPlayer.getNumber())) {
-                    game.setNameWinner(secondPlayer.getName());
+                    game.setWinner(secondPlayer.getName());
                     break;
                 }
             }
-            System.out.println("Выйграл игрок - " + game.getNameWinner());
+            System.out.println("Выйграл игрок - " + game.getWinner());
 
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]: ");
                 playerAnswer = in.next();
-            }
-            while (!"yes".equals(playerAnswer) && !"no".equals(playerAnswer));
+            } while (!"yes".equals(playerAnswer) && !"no".equals(playerAnswer));
         }
     }
 }
