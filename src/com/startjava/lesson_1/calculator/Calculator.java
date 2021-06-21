a0aed9eb2309828fdc3edc57138871df8bc73224
Ilya20011982 <ilya_fr@mail.ru>
2021-06-21 21:28:20 +0500

    add

--- src/com/startjava/lesson_1/calculator/Calculator.java
+++ src/com/startjava/lesson_1/calculator/Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CalculatorOld {
+public class Calculator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите первое число - ");
